fdf5fb36cb9fd90343f491ea69c0d7a7c5698771
David Turton <dturton@etrivo.com>
2019-03-21 10:57:15 -0400

    v1.0.3

--- src/loqate.ts
+++ src/loqate.ts
@@ -4,9 +4,13 @@ import { IOptions, IParams, IRequestOptions } from './interfaces';
 const loqate = ({ key, countries = [] }: IOptions) => {
   const config = { key };
   const client = axios.create({ baseURL: 'https://api.addressy.com/Capture/Interactive/' });
+  const localFetch = (endPoint: string, requestOptions: IRequestOptions) => {
+    requestOptions.params.Key = config.key;
+    return client.get(`${endPoint}/v1.00/json3.ws`, requestOptions);
+  };
   return {
     async searchAddresses(query: string) {
-      const response = await this.localFetch('Find', {
+      const response = await localFetch('Find', {
         params: {
           Countries: countries,
           IsMiddleware: true,
@@ -17,16 +21,12 @@ const loqate = ({ key, countries = [] }: IOptions) => {
       return response.data.Items.filter((item: { Type: string }) => item.Type === 'Address');
     },
     async getAddress(addressId: number) {
-      const response = await this.localFetch('Retrieve', {
+      const response = await localFetch('Retrieve', {
         params: {
           Id: addressId,
         },
       });
       return response.data.Items[0];
-    },
-    async localFetch(endPoint: string, requestOptions: IRequestOptions) {
-      requestOptions.params.Key = config.key;
-      return client.get(`${endPoint}/v1.00/json3.ws`, requestOptions);
     },
   };
 };
